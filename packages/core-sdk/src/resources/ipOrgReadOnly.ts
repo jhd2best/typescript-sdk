1d89663ea59a458dbefe470dbd609c6b2653fc75
lutty <lutty@lutty.me>
2023-11-22 15:23:48 +0800

    fix bug

--- packages/core-sdk/src/resources/ipOrgReadOnly.ts
+++ packages/core-sdk/src/resources/ipOrgReadOnly.ts
@@ -1,7 +1,12 @@
 import { AxiosInstance } from "axios";
 import { isAddress, PublicClient } from "viem";
 
-import { GetIPOrgRequest, GetIPOrgResponse, ListIPOrgResponse } from "../types/resources/IPOrg";
+import {
+  GetIPOrgRequest,
+  GetIPOrgResponse,
+  ListIPOrgRequest,
+  ListIPOrgResponse,
+} from "../types/resources/IPOrg";
 import { handleError } from "../utils/errors";
 import { QueryOptions } from "../types/options";
 
@@ -41,9 +46,9 @@ export class IPOrgReadOnlyClient {
    *
    * @returns the response object that contains a list of IPOrgs
    */
-  public async list(options?: QueryOptions): Promise<ListIPOrgResponse> {
+  public async list(request?: ListIPOrgRequest): Promise<ListIPOrgResponse> {
     try {
-      const response = await this.httpClient.post(`/protocol/iporg`, options);
+      const response = await this.httpClient.post(`/protocol/iporg`, request);
       return response.data as ListIPOrgResponse;
     } catch (error: unknown) {
       handleError(error, "Failed to list IPOrgs.");
